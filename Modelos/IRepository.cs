75add350b3be70e7c7b6ad2d374213150f930fcc
Jesus <tordek.coast@gmail.com>
2010-09-25 19:25:18 -0300

    Adicionado pasta DAO e mudana de arquivo

---
+++ Modelos/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Modelos
+{
+    public interface IRepository<T>
+    {
+        T Inserir(T klass);
+        T Atualizar(T klass);
+        void Deletar(T klass);
+    }
+}
---
+++ Modelos/Usuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Modelos
+{
+    public class Usuario : UsuarioDAO
+    {
+        private String nm_nome, nm_login, nm_senha;
+        private Int32 id_tp_usuario;
+
+        public Int32 TipoUsuario
+        {
+            get { return id_tp_usuario; }
+            set { id_tp_usuario = value; }
+        }
+
+        public String Senha
+        {
+            get { return nm_senha; }
+            set { nm_senha = value; }
+        }
+
+        public String Login
+        {
+            get { return nm_login; }
+            set { nm_login = value; }
+        }
+
+        public String Nome
+        {
+            get { return nm_nome; }
+            set { nm_nome = value; }
+        }
+
+    }
+}
